fa6b4138654fccfebda6599478e48eeeb204e07c
Raí Vieira Adriano <raivieiraadriano92@gmail.com>
2018-04-24 13:05:51 -0300

    start

---
+++ app/routes.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+const routes = express.Router();
+
+const authMiddleware = require('./middlewares/auth');
+const guestMiddleware = require('./middlewares/guest');
+
+const authController = require('./controllers/authController');
+const dashboardController = require('./controllers/dashboardController');
+
+routes.use((req, res, next) => {
+  res.locals.flashSuccess = req.flash('success');
+  res.locals.flashError = req.flash('error');
+  next();
+});
+
+routes.get('/', guestMiddleware, authController.signin);
+routes.get('/signup', guestMiddleware, authController.signup);
+routes.get('/signout', authController.signout);
+
+routes.post('/register', authController.register);
+routes.post('/authenticate', authController.authenticate);
+
+routes.use('/app', authMiddleware);
+
+routes.get('/app/dashboard', dashboardController.index);
+
+routes.use((req, res) => res.render('erros/404'));
+
+routes.use((err, req, res, _next) => {
+  res.status(err.status || 500);
+
+  return res.render('erros/index', {
+    message: err.message,
+    error: process.env.NODE_ENV === 'production' ? {} : err,
+  });
+});
+
+module.exports = routes;
---
+++ config/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+  username: 'root',
+  password: null,
+  database: 'snippetfy',
+  host: 'db',
+  dialect: 'mysql',
+};
